455ba03d539173230da0bcfbe976b5dbfb563711
hoanhvph37867 <127205035+hoanhvph37867@users.noreply.github.com>
2024-08-28 09:34:19 +0700

    hoan

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const { createServer } = require('node:http');
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config()
 
-const hostname = '127.0.0.1';//=localhost
-const port = 3000;
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+const port = process.env.PORT
+const hostname = process.env.HOST_NAME
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+//khai bao router
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+/////////////////////// config template engine
+app.set('views', './src/views/')
+app.set('view engine', 'ejs')
+//////////////////////
+
+///////config static file
+app.use(express.static(path.join(__dirname, './src/public')))
+///////////
+
+app.get('/res', (req, res) => {
+    //res.send('Hoi sinh')
+    res.render('sample.ejs')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+})
